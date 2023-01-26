50d9c1d8f4bad307b684b01a6e290909e4763178
neema <neemadas@gmail.com>
2023-01-26 23:47:11 +0000

    fix: Add async/await, try{} and catch{}
    Added async/await to fetch data from an API. Also, added ( try - catch ) to catch errors and then handle it.
    
    Added-by: Neema Sheby
    Added-on: 26-06-2023

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,26 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
+
+// fetchData(jsonTypicode);
+
+const fetchData = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    if (!response.ok)
+      throw new Error(" ❌ Something went wrong with fetching data!");
+
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 fetchData(jsonTypicode);
